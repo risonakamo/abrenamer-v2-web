c14855b47b1eff4ddf91e5afacc1b74b31fe77ab
risonakamo <hmykanae@gmail.com>
2024-08-25 16:04:05 -0400

    setting up to style rename rule selector

---
+++ stories/rename-rule-selector.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+import RenameRuleSelector from "@/components/rename-rule-selector/rename-rule-selector.svelte";
+
+export const meta={
+    title:"rename selector",
+    component:RenameRuleSelector
+};
+</script>
+
+<script>
+import {Story,Template} from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+    <RenameRuleSelector {...args}/>
+</Template>
+
+<Story name="normal" args={{
+
+}}/>
