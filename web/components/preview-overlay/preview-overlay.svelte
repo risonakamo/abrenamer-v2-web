146012904fb6cdaa3ea9551e33cb15fab1dc5510
risonakamo <hmykanae@gmail.com>
2024-09-22 11:01:11 -0400

    starting key event handler

--- web/components/preview-overlay/preview-overlay.svelte
+++ web/components/preview-overlay/preview-overlay.svelte
@@ -2,13 +2,18 @@
 export var imgSrc:string|undefined=undefined;
 export var imgCounterNum:number=0;
 export var imgCounterMax:number=0;
+
+function h_keyControl(e:KeyboardEvent):void
+{
+
+}
 </script>
 
 <style lang="sass">
     @import "./preview-overlay.sass"
 </style>
 
-<div class="preview-overlay">
+<div class="preview-overlay" on:keypress={h_keyControl}>
     <div class="img-counter">
         {imgCounterNum}/{imgCounterMax}
     </div>
