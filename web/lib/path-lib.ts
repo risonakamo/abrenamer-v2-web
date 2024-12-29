1f243dc8838ef61954ef28426f702754c861fd59
risonakamo <hmykanae@gmail.com>
2024-12-29 16:18:33 -0500

    jpeg is image

--- web/lib/path-lib.ts
+++ web/lib/path-lib.ts
@@ -14,6 +14,7 @@ export function isImage(itemPath:string):boolean
     {
         case ".png":
         case ".jpg":
+        case ".jpeg":
         case ".gif":
         case ".webp":
         return true;
